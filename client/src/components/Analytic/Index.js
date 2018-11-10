6a2d20310bdc610f59481d9b2cbbea67cb8a6502
glautrou <g.lautrou@outlook.com>
2018-11-10 16:04:03 +0100

    Create Analytic component

---
+++ client/src/components/Analytic/Index.js
@@ -0,0 +1,20 @@
+// @flow
+
+import React, { Component } from "react";
+
+// function test(x: ?number): string {
+//   if (x) {
+//     return x;
+//   }
+//   return "default string";
+// }
+
+type Props = {};
+
+class Analytic extends Component<Props> {
+  render() {
+    return <div>Analytics</div>;
+  }
+}
+
+export default Analytic;
