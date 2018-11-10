bbe5f436ea1d3abe45bcf3ac88c95a07c21fe601
glautrou <g.lautrou@outlook.com>
2018-11-10 21:10:56 +0100

    Improve footer

--- client/src/components/App/Footer.js
+++ client/src/components/App/Footer.js
@@ -2,17 +2,34 @@
 
 import React, { Component } from "react";
 import Grid from "@material-ui/core/Grid";
+import Typography from "@material-ui/core/Typography";
+import { withStyles } from "@material-ui/core/styles";
 
-type Props = {};
+const styles = theme => ({
+  footer: {
+    backgroundColor: "#3f51b5",
+    padding: theme.spacing.unit * 2
+  }
+});
+
+type Props = { classes: any };
 
 function Footer(props: Props) {
   return (
-    <Grid container spacing={24}>
-      <Grid item xs={12}>
+    <footer className={props.classes.footer}>
+      <Typography variant="h6" align="center" gutterBottom>
+        Azure DevOps Analytics
+      </Typography>
+      <Typography
+        variant="subtitle1"
+        align="center"
+        color="textSecondary"
+        component="p"
+      >
         Copyright
-      </Grid>
-    </Grid>
+      </Typography>
+    </footer>
   );
 }
 
-export default Footer;
+export default withStyles(styles)(Footer);
