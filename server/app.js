e5f1b0c2cd644af054b6c55b08e50eeab45321f7
glautrou <g.lautrou@outlook.com>
2018-11-11 08:56:16 +0100

    Display more detailled logs for production

--- server/app.js
+++ server/app.js
@@ -14,7 +14,12 @@ var app = express();
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "hbs");
 
-app.use(logger("dev"));
+if (app.get("env") === "production") {
+  app.use(logger("combined"));
+} else {
+  app.use(logger("dev"));
+}
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
