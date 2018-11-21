360abda13f574766cbaa67b9930256fca68691db
Gilles Lautrou <g.lautrou@outlook.com>
2018-11-21 17:54:10 +0100

    Chnage from BrowserRouter to HasRouter for static file hosting

--- client/src/components/App/AppRouter.js
+++ client/src/components/App/AppRouter.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
+import { HashRouter as Router, Route, Switch, Link } from 'react-router-dom';
 import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
 import red from '@material-ui/core/colors/red';
 import indigo from '@material-ui/core/colors/indigo';
